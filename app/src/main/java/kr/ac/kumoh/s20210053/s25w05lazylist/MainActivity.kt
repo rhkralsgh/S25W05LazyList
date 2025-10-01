360047e5b2fa509fe316b430854e87e51d0f0d54
rhkralsgh <kmh23937@gmail.com>
2025-10-01 15:10:22 +0900

    🚧LazyColumn 사용

--- app/src/main/java/kr/ac/kumoh/s20210053/s25w05lazylist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20210053/s25w05lazylist/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.s20210053.s25w05lazylist.ui.theme.S25W05LazyListTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             S25W05LazyListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        MyList(
+        modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    S25W05LazyListTheme {
-        Greeting("Android")
+fun SongItem(index: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc))
+            .padding(16.dp)
+    ) {
+        TextTitle("노래 $index")
+        TextSinger("이 노래를 부른 가수는 $index 입니다")
+    }
+}
+
+@Composable
+fun TextTitle(title: String) {
+    Text(title, fontSize = 30.sp)
+}
+
+@Composable
+fun TextSinger(singer: String) {
+    Text(singer, fontSize = 20.sp)
+}
+
+@Composable
+fun MyList(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp)
+    ) {
+        items(30) {
+            SongItem(it)
+        }
     }
 }
