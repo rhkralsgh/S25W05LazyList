63f64de2d1d87f752a2d00bdcacf364008e19913
rhkralsgh <kmh23937@gmail.com>
2025-10-01 14:31:54 +0900

    🎉Project 시작

---
+++ app/src/main/java/kr/ac/kumoh/s20210053/s25w05lazylist/MainActivity.kt
@@ -0,0 +1,47 @@
+package kr.ac.kumoh.s20210053.s25w05lazylist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.s20210053.s25w05lazylist.ui.theme.S25W05LazyListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S25W05LazyListTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S25W05LazyListTheme {
+        Greeting("Android")
+    }
+}
